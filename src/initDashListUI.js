caf94db8790509f2423218442edcd594f686854a
Raihan C <139701695+raihanCarder@users.noreply.github.com>
2025-07-18 02:19:11 -0400

    fix bug

--- src/initDashListUI.js
+++
@@ -1,7 +0,0 @@
-export default function initDashListUI() {
-    const dashboardList = document.getElementById("dashboard-lists");
-    const listP = document.createElement("p");
-    listP.textContent = "My Lists:";
-
-    dashboardList.appendChild(listP);
-}
